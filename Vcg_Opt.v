edbeeb49a112cad6ac11a8f941623e90ddd82581
Lionel Blatter <lionel.blatter@kit.edu>
2021-03-31 12:50:14 +0200

    Add proof for a optimized verification condition generator

--- Vcg_Opt.v
+++ Vcg_Opt.v
@@ -27,316 +27,322 @@ destruct H.
   * auto.
 Qed.
 
-Fixpoint tc (c : com) (m : Sigma.sigma)
-            (cl: Phi.phi) (suite : Sigma.sigma -> Prop) : Prop :=
+Fixpoint tc (c: com) (m m': Sigma.sigma)
+            (cl: Phi.phi) (fin: Prop -> Prop): Prop :=
     match c with
-    | CSkip => forall m', m = m' -> suite m'
-    | CAss x a => forall m', (m' = set m x (aeval m a)) -> suite m'
-    | CAssert b => forall m', b m -> m = m' -> suite m'
-    | CSeq p1 p2 => tc p1 m cl (fun m' => tc p2 m' cl suite)
+    | CSkip => fin (m = m')
+    | CAss x a => fin (m' = set m x (aeval m a))
+    | CAssert b =>  fin (b m /\ m = m')
+    | CSeq p1 p2 => forall m'', 
+        tc p1 m m'' cl (fun f1 => tc p2 m'' m' cl (fun f2 => fin (f1 /\ f2)))
     | CIf b p1 p2 =>
-               tc p1 m cl (fun m1' =>
-               tc p2 m cl (fun m2' =>
-               forall m',
-               branch (bassn b m) (m' = m1') (m' = m2') -> suite m'))
-    | CWhile b p inv => inv m ->
-                        forall m', inv m' -> beval m' b = false -> suite m'
-    | CCall f => (get_pre (cl f)) m ->
-                  forall m',  (get_post (cl f)) m' -> suite m'
+        tc p1 m m' cl (fun p1 => 
+        tc p2 m m' cl (fun p2 => 
+            fin (branch (bassn b m) p1 p2)))
+    | CWhile b p inv => fin (inv m /\ inv m' /\ beval m' b = false)
+    | CCall f => fin((get_pre (cl f)) m /\ (get_post (cl f)) m')
     end.
 
 Lemma consequence_tc_suite :
-forall p cl m (suite1 suite2 : Sigma.sigma -> Prop),
-(forall s, suite1 s -> suite2 s) ->
-tc p m cl suite1 -> tc p m cl suite2.
+forall p cl m m' (suite1 suite2 : Prop -> Prop),
+(forall p, suite1 p -> suite2 p) ->
+tc p m m' cl suite1 -> tc p m m' cl suite2.
 Proof.
 intros p cl.
 induction p.
-  * intros. simpl. simpl in H0.
-    intros.
+  * simpl. intros.
     apply H.
     apply H0.
-    assumption.
-  * intros. simpl. simpl in H0.
-    intros.
+  * simpl. intros.
     apply H.
     apply H0.
-    assumption.
-  * intros. simpl. simpl in H0.
-    intros.
+  * simpl. intros.
     apply H.
     apply H0.
-    assumption.
-    assumption.
-  * intros.  simpl. simpl in H0.
-    eapply IHp1.
+  * simpl. intros.
+    eapply IHp1 with (fun p => tc p2 m'' m' cl (fun f2 : Prop => suite1 (p /\ f2))).
     - intros.
-      eapply IHp2.
-       + apply H.
+      eapply IHp2 with (fun f => suite1(p /\ f)).
+       + intros. apply H. assumption.
        + apply H1.
-    - assumption.
-  * intros. simpl. simpl in H0.
-    intros.
-    eapply IHp1.
+    - apply H0.
+  * simpl. intros.
+    eapply IHp1 with 
+     (fun p => tc p2 m m' cl (fun f2 : Prop => suite1 (branch (bassn b m) p f2))).
     - intros.
-      eapply IHp2.
-       + intros. apply H.
-        generalize H3. generalize m'. apply H2.
-       + apply H1. 
-     - apply H0.
-  * intros. simpl. simpl in H0.
-    intros. apply H.
+      eapply IHp2 with (fun f => suite1 (branch (bassn b m) p f)).
+       + intros. apply H. assumption.
+       + apply H1.
+    - apply H0.
+  * simpl. intros. 
+    apply H.
     apply H0.
-    all : try assumption.
-  * intros. simpl. simpl in H0.
-    intros. apply H.
+  * simpl. intros. 
+    apply H.
     apply H0.
-    all : try assumption.
 Qed.
 
-Lemma split_tc :
-forall p m cl (suite1 suite2 : Sigma.sigma -> Prop),
-tc p m cl (fun m' => suite1 m' /\ suite2 m') ->
-tc p m cl suite1 /\ tc p m cl suite2.
+Lemma intros_tc :
+forall p m m' cl (debut : Prop) (suite : Prop -> Prop),
+tc p m m' cl (fun f => debut -> suite f) ->
+(debut -> tc p m m' cl suite).
 Proof.
-intros.
-split.
-* apply (consequence_tc_suite _ _ _ (fun m' => suite1 m' /\ suite2 m')).
+induction p;simpl.
+* intros. apply H. assumption.
+* intros. apply H. assumption.
+* intros. apply H. assumption.
+* intros.  
+  eapply consequence_tc_suite.
   + intros.
-    destruct H0.
-    assumption.
-  + assumption.
-* apply (consequence_tc_suite _ _ _ (fun m' => suite1 m' /\ suite2 m')).
+    generalize H0.
+    apply IHp2.
+    apply H1.
+  + apply H.
+* intros.
+  eapply consequence_tc_suite.
   + intros.
-    destruct H0.
-    assumption.
-  + assumption.
+    generalize H0.
+    apply IHp2.
+    apply H1.
+  + apply H.
+* intros. apply H. assumption.
+* intros. apply H. assumption.
+Qed.
+
+Lemma prenexe_tc :
+forall p m m' cl (suite : sigma -> Prop -> Prop),
+(forall s, tc p m m' cl (suite s)) ->
+tc p m m' cl (fun f => forall s,suite s f).
+Proof.
+induction p;simpl.
+* intros. apply H.
+* intros. apply H.
+* intros. apply H.
+* intros.
+  eapply consequence_tc_suite.
+  + intros p He.
+    apply IHp2.
+    apply He.
+  + apply IHp1.
+    intros.
+   apply H.
+* intros.
+  eapply consequence_tc_suite.
+  + intros p He.
+    apply IHp2.
+    apply He.
+  + apply IHp1.
+    intros.
+   apply H.
+* intros. apply H.
+* intros. apply H.
 Qed.
 
 Lemma opt_1_true:
-forall p1 p2 b m cl (suite : Sigma.sigma -> Prop),
+forall p1 p2 b m m' cl (suite : Prop),
 bassn b m ->
-tc p1 m cl (fun m1' => 
-tc p2 m cl (fun m2' =>  forall m', branch (bassn b m) (m' = m1') (m' = m2') -> suite m')) ->
-tc p1 m cl (fun m1' => 
-tc p2 m cl (fun m2' =>  forall m', m' = m1' -> suite m')).
+tc p1 m m' cl (fun f1 => 
+tc p2 m m' cl (fun f2 =>  branch (bassn b m) f1 f2 -> suite)) ->
+tc p1 m m' cl (fun f1 => 
+tc p2 m m' cl (fun f2 => f1 -> suite)).
 Proof.
 intros.
-apply (consequence_tc_suite _ _ _ (fun m1' => tc p2 m cl (fun m2' => forall m', branch (bassn b m) (m' = m1') (m' = m2') -> suite m'))).
+apply (consequence_tc_suite _ _ _ _ 
+             (fun f1 => tc p2 m m' cl (fun f2 => 
+              branch (bassn b m) f1 f2 -> suite))).
 * intros.
-  apply (consequence_tc_suite _ _ _ (fun m2' : sigma => forall m' : sigma, branch (bassn b m) (m' = s) (m' = m2') -> suite m')).
-  -  intros. apply H2.
+  apply (consequence_tc_suite _ _ _ _ 
+        (fun f2 => branch (bassn b m) p f2 -> suite)).
+  - intros. apply H2.
     apply Then.
-    assumption.
-    assumption.
+    all: try assumption.
   - assumption.
 * assumption.
 Qed.
 
 Lemma opt_1_false:
-forall p1 p2 b m cl (suite : Sigma.sigma -> Prop),
+forall p1 p2 b m m' cl (suite : Prop),
 ~bassn b m ->
-tc p1 m cl (fun m1' => 
-tc p2 m cl (fun m2' =>  forall m', branch (bassn b m) (m' = m1') (m' = m2') -> suite m')) ->
-tc p1 m cl (fun m1' => 
-tc p2 m cl (fun m2' =>  forall m', m' = m2' -> suite m')).
+tc p1 m m' cl (fun f1 => 
+tc p2 m m' cl (fun f2 =>  branch (bassn b m) f1 f2 -> suite)) ->
+tc p1 m m' cl (fun f1 => 
+tc p2 m m' cl (fun f2 => f2 -> suite)).
 Proof.
 intros.
-apply (consequence_tc_suite _ _ _ (fun m1' => tc p2 m cl (fun m2' => forall m', branch (bassn b m) (m' = m1') (m' = m2') -> suite m'))).
+apply (consequence_tc_suite _ _ _ _ 
+             (fun f1 => tc p2 m m' cl (fun f2 => 
+              branch (bassn b m) f1 f2 -> suite))).
 * intros.
-  apply (consequence_tc_suite _ _ _ (fun m2' : sigma => forall m' : sigma, branch (bassn b m) (m' = s) (m' = m2') -> suite m')).
-  -  intros. apply H2.
+  apply (consequence_tc_suite _ _ _ _ 
+        (fun f2 => branch (bassn b m) p f2 -> suite)).
+  - intros. apply H2.
     apply Else.
-    assumption.
-    assumption.
+    all: try assumption.
   - assumption.
 * assumption.
 Qed.
 
-Lemma opt_2_true:
-forall p1 p2 m cl (suite : Sigma.sigma -> Prop),
-tc p1 m cl (fun m1' => 
-tc p2 m cl (fun m2' =>  forall m',  m' = m1' -> suite m')) ->
-tc p1 m cl (fun m1' => 
-tc p2 m cl (fun m2' =>  suite m1')).
+Lemma simpl_tc :
+forall p m m' cl (suite : Prop),
+tc p m m' cl (fun _ => suite) -> suite .
+Proof.
+induction p;simpl.
+* intros. apply H.
+* intros. apply H.
+* intros. apply H.
+* intros.
+  assert (H1: tc p1 m m cl (fun _ : Prop => suite)).
+  { intros.
+    eapply consequence_tc_suite with 
+    ((fun _ : Prop => tc p2 m m' cl (fun _ : Prop => suite))).
+    + intros.
+      eapply IHp2.
+      apply H0.
+    + apply H.
+  }
+  eapply IHp1.
+  apply H1.
+* intros.
+  eapply IHp2.
+  eapply IHp1.
+  apply H.
+* intros. apply H.
+* intros. apply H.
+Qed.
+
+Lemma tc_same :
+forall p cl m (suite1 : Sigma.sigma -> Prop),
+(forall m', tc p m m' cl (fun p => p -> suite1 m')) -> Vcg.tc p m cl suite1.
 Proof.
 intros.
-apply (consequence_tc_suite _ _ _ (fun m1' => 
-                                      tc p2 m cl (fun m2' =>  forall m',  m' = m1'-> suite m'))).
+generalize dependent suite1.
+generalize dependent m.
+induction p;simpl.
+* intros. apply H. assumption.
+* intros. apply H. assumption.
+* intros. apply H. auto.
+* intros.
+  eapply Vcg.consequence_tc_suite.
   + intros.
-    eapply consequence_tc_suite in H0.
-    - apply H0.
-    - simpl. intros.
-      apply H1.
-        reflexivity.
-  + assumption.
+    apply IHp2.
+    apply H0.
+  + apply IHp1.
+    intros.
+    eapply consequence_tc_suite.
+    - intros p He Hp m'0.
+      generalize dependent Hp.
+      generalize dependent m'0.
+      apply He.
+    - apply prenexe_tc.
+      intros.
+      eapply consequence_tc_suite.
+      ** intros p He.
+         apply intros_tc.
+         eapply consequence_tc_suite with 
+         (fun f : Prop => p /\ f -> suite1 s).
+         ++ auto.
+         ++ apply He.
+      ** apply H.
+* intros.
+  split;intros.
+  + apply IHp1.
+    intros.
+    specialize (H m').
+    specialize (opt_1_true p1 p2 b m m' cl (suite1 m') H0 H).
+    intros.
+    eapply consequence_tc_suite.
+    - intros p He.
+      apply (simpl_tc p2 m m' cl (p -> suite1 m')).
+      apply He.
+    - apply H1.
+  + apply IHp2.
+    intros.
+    specialize (H m').
+    specialize (opt_1_false p1 p2 b m m' cl (suite1 m') H0 H).
+    intros.
+    apply (simpl_tc p1 m m' cl (tc p2 m m' cl (fun p : Prop => p -> suite1 m'))).
+    apply H1.
+* intros.
+  apply H.
+  auto.
+* intros.
+  apply H.
+  auto.
 Qed.
 
-Lemma opt_2_false:
-forall p1 p2 m cl (suite : Sigma.sigma -> Prop),
-tc p1 m cl (fun m1' => 
-tc p2 m cl (fun m2' =>  forall m',  m' = m2' -> suite m')) ->
-tc p1 m cl (fun m1' => 
-tc p2 m cl (fun m2' =>  suite m2')).
-Proof.
-intros.
-apply (consequence_tc_suite _ _ _ (fun m1' => 
-                                      tc p2 m cl (fun m2' =>  forall m',  m' = m2'-> suite m'))).
-  + intros.
-    eapply consequence_tc_suite in H0.
-    - apply H0.
-    - simpl. intros.
-      apply H1.
-        reflexivity.
-  + assumption.
-Qed.
-
-Lemma tc_split :
-forall p cl m (suite1 suite2 : Sigma.sigma -> Prop),
-tc p m cl suite1 -> tc p m cl suite2 ->
-tc p m cl (fun m' => suite1 m' /\ suite2 m').
-Proof.
-intros p cl.
-induction p.
-+ simpl. intros.
-  split.
-  apply H. subst. reflexivity.
-  apply H0. subst. reflexivity.
-+ simpl. intros.
-  split.
-  apply H. subst. reflexivity.
-  apply H0. subst. reflexivity.
-+ simpl. intros.
-  split.
-  apply H. assumption. subst. reflexivity.
-  apply H0. assumption. subst. reflexivity.
-+ simpl. intros.
-  apply (consequence_tc_suite _ _ _ (fun m => tc p2 m cl suite1 /\ tc p2 m cl suite2)).
-  * intros. destruct H1. apply IHp2. assumption. assumption.
-  * apply IHp1. assumption. assumption.
-+ intros.
-  simpl.
-  destruct (beval m b) eqn:H1.
-  * apply bexp_eval_true in H1.
-    simpl in H, H0.
-    apply opt_1_true in H.
-    apply opt_1_true in H0.
-    apply opt_2_true in H.
-    apply opt_2_true in H0.
-    apply (consequence_tc_suite _ _ _ 
-        (fun m' => tc p2 m cl ( fun _ => suite1 m' /\ suite2 m'))).
-        - intros s. apply consequence_tc_suite.
-          intros. destruct H3.
-          ++ subst. assumption.
-          ++ contradiction H3.
-        - apply (consequence_tc_suite _ _ _ 
-        (fun m' => tc p2 m cl ( fun _ => suite1 m') /\ tc p2 m cl ( fun _ => suite2 m'))).
-          ++ intros.  destruct H2. apply IHp2. assumption. assumption.
-          ++ apply IHp1. assumption. assumption.
-        - assumption.
-        - assumption.
-  * apply bexp_eval_false in H1.
-    simpl in H, H0.
-    apply opt_1_false in H.
-    apply opt_1_false in H0.
-    apply opt_2_false in H.
-    apply opt_2_false in H0.
-    apply (consequence_tc_suite _ _ _ 
-        (fun _ => tc p2 m cl ( fun m' => suite1 m' /\ suite2 m'))).
-        - intros s. apply consequence_tc_suite.
-          intros. destruct H3.
-          ++ contradiction H3.
-          ++ subst. assumption.
-        - apply (consequence_tc_suite _ _ _ 
-        (fun _ => tc p2 m cl ( fun m' => suite1 m') /\ tc p2 m cl ( fun m' => suite2 m'))).
-          ++ intros.  destruct H2. apply IHp2. assumption. assumption.
-          ++ apply IHp1. assumption. assumption.
-        - assumption.
-        - assumption.
-+ simpl. intros.
-  split. 
-    * apply H.
-      all: try assumption.
-    * apply H0.
-      all: try assumption.
-+ simpl. intros.
-  split.
-    * apply H.
-      all: try assumption.
-    * apply H0.
-      all: try assumption.
-Qed.
-
-Fixpoint tc' (c : com) (m : Sigma.sigma)
+Fixpoint tc' (c : com) (m: Sigma.sigma)
             (cl: Phi.phi) : Prop :=
-    match c with
-    | CSkip => True
-    | CAss x a => True
-    | CAssert b => b m
-    | CSeq p1 p2 => tc' p1 m cl /\
-                    tc p1 m cl (fun m' => tc' p2 m' cl)
-    | CIf b p1 p2 =>
+match c with
+ | CSkip => True
+ | CAss x a => True
+ | CAssert b => b m
+ | CSeq p1 p2 => tc' p1 m cl /\
+                 forall m'',
+                 tc p1 m m'' cl (fun f => f -> tc' p2 m'' cl)
+ | CIf b p1 p2 =>
       (bassn b m -> tc' p1 m cl) /\ (~bassn b m -> tc' p2 m cl)
-   | CWhile b p inv => inv m /\
-                          (forall m', bassn b m' -> tc' p m' cl) /\
-                          (forall m', inv m'  -> tc p m' cl inv)
-   | CCall f => (get_pre (cl f)) m
-    end.
+ | CWhile b p inv => inv m /\
+                    (forall m', bassn b m' -> tc' p m' cl) /\
+                    (forall m' m'', inv m'  -> tc p m' m'' cl (fun f => f -> inv m''))
+ | CCall f => (get_pre (cl f)) m
+end.
+
+Lemma tc'_same :
+forall p cl m,
+tc' p m cl -> Vcg.tc' p m cl.
+Proof.
+induction p; simpl.
+* intros. auto.
+* intros. auto.
+* intros.  apply H.
+* intros.
+  split.
+  - apply IHp1.
+    apply H.
+  - apply tc_same.
+    intros.
+    eapply consequence_tc_suite with
+    (fun f : Prop => f -> tc' p2 m' cl).
+    + intros.
+      apply IHp2.
+      auto.
+    + apply H.
+* intros.
+  split;intros.
+  - apply IHp1.
+    apply H. assumption.
+  - apply IHp2.
+    apply H. assumption.
+* intros.
+  split.
+  - apply H.
+  - split;intros.
+    + apply IHp.
+      apply H.
+      assumption.
+    + apply tc_same.
+      intros.
+      apply H.
+      assumption.
+* intros. apply H.
+Qed.
 
 Definition tc_p (ps: Psi.psi) (cl : Phi.phi) : Prop :=
-    forall f m, (get_pre (cl f)) m -> tc' (ps f) m cl /\
-                tc (ps f) m cl (get_post (cl f)).
+    forall f m m', (get_pre (cl f)) m -> tc' (ps f) m cl /\
+                tc (ps f) m m' cl (fun p => p -> (get_post (cl f)) m').
 
-  (* use relational properties : module Opt to an proper file*)
-
-(*Lemma simpl_tc :
-forall p cl suite, forall m, tc p m cl (fun _ => suite) -> suite.
+Lemma tc_p_same :
+forall ps cl,
+tc_p ps cl -> Vcg.tc_p ps cl.
 Proof.
-intros p cl suite.
-induction p.
-* simpl in H. specialize (H m). apply H. reflexivity.
-* simpl in H. specialize (H (set m x (aeval m a))). apply H. reflexivity.
-* simpl in H. specialize (H m). apply H. apply H. reflexivity.
-* simpl in H, H0.
-  destruct H.
-   specialize 
-    (tc_split p1 cl m (fun m' : sigma => tc' p2 m' cl) (fun m' : sigma => tc p2 m' cl (fun _ : sigma => suite))).
-     intros.
-     specialize (H2 H1 H0).
-  eapply (consequence_tc_suite _ _ _ _ (fun m' : sigma => tc' p2 m' cl /\suite)) in H2.
-  + apply split_tc in H2.
-    destruct H2.
-    eapply IHp1.
-    - apply H.
-    - apply H3.
-  + intros. destruct H3. split.
-     - assumption.
-     - eapply IHp2.
-      ** apply H3.
-      ** assumption. 
-* simpl in H, H0.
- destruct (beval m b) eqn:H1.
-  + apply bexp_eval_true in H1.
-    apply opt_1_true in H0.
-    apply opt_2_true in H0.
-    eapply (consequence_tc_suite _ _ _ _ (fun _ : sigma => suite)) in H0.
-    - eapply IHp1. apply H. apply H1. apply H0.
-    -
-    Abort.
- (*    apply H0.
-    - intros. eapply IHp2. apply H. 
- 
- 
-        apply H1.  apply H0.
-    - assumption.
-   + apply bexp_eval_false in H1.
-    apply opt_1_false in H.
-    apply opt_2_false in H.
-    eapply (consequence_tc_suite _ _ _ _ (fun _ : sigma => suite)) in H.
-    - eapply IHp1. apply H.
-    - intros. eapply IHp2. apply H0.
-    - assumption.
-Qed.*)
-*)
-
-
+intros.
+unfold Vcg.tc_p.
+split;intros.
+* apply tc'_same.
+  apply H.
+  all: try assumption.
+* apply tc_same.
+  intros.
+  apply H.
+  apply H0.
+Qed.
