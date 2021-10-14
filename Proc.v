d3877e6e749581cf3fabe216925e39015c6c0512
Lionel Blatter <lionel.blatter@kit.edu>
2021-10-14 18:23:06 +0200

    Fix comment

--- Proc.v
+++ Proc.v
@@ -2,7 +2,7 @@ Require Import Arith.
 
 (** We choose procedure names to be natural numbers.
     However, the best would be to define Proc as a
-    Module Type OrderedType and make all proof modular.
+    Module Type DecidableType and make all proof modular.
 **)
 
 (** Procedure names are Natural **)
