9ea14e46bf1670a6ec078d9e2c484335f37d4911
Lionel Blatter <lionel.blatter@mpi-sp.org>
2024-04-08 17:47:22 +0200

    Some nicer code

--- Correct_Rela.v
+++ Correct_Rela.v
@@ -21,8 +21,8 @@ forall (P: r_precondition) (Q: r_postcondition),
 (forall ps,
   (forall ml (hy:length p = length ml),
      P ml -> tr rcl ps -> rtc' p ml h (phi_call (extract rcl) ps) hy hyh) /\
-  (forall ml (hy:length p = length ml), 
-     P ml -> tr rcl ps -> 
+  (forall ml (hy:length p = length ml),
+     P ml -> tr rcl ps ->
      rtc p ml h (phi_call (extract rcl) ps ) (fun ml' _ => Q ml' ml) hy hyh)) ->
 relational_prop_ctx rcl ps P Q p.
 Proof.
@@ -59,7 +59,7 @@ induction p;intros.
    destruct h;[ discriminate hyh | ].
    inversion H4;subst.
    inversion hyh.
-   specialize (IHp (h0) H6 (fun sl => P (s :: sl)) 
+   specialize (IHp (h0) H6 (fun sl => P (s :: sl))
                            (fun sl' sl => Q (s1 :: sl') (s :: sl))).
    simpl in IHp.
    generalize H13.
@@ -73,7 +73,7 @@ induction p;intros.
        assert (hy2: length (a ::p) = length (s::ml)).
        {intros. simpl. rewrite hy. reflexivity. }
        specialize (H (s :: ml) hy2 H5).
-       destruct (mk_rtc'_def a p (phi_call (extract rcl) ps) s ml h h0 hy2 hyh) 
+       destruct (mk_rtc'_def a p (phi_call (extract rcl) ps) s ml h h0 hy2 hyh)
           as (hyr1 & hyr2 & HYP).
        rewrite HYP in H.
        destruct H.
@@ -98,14 +98,14 @@ induction p;intros.
           assert (hy2: length (a ::p) = length (m::ml)).
           {intros. simpl. rewrite hy. reflexivity. }
           specialize (H (m :: ml) hy2 H12 Hproc).
-          destruct (mk_rtc'_def a p (phi_call (extract rcl)ps) m ml h h0 hy2 hyh) 
+          destruct (mk_rtc'_def a p (phi_call (extract rcl)ps) m ml h h0 hy2 hyh)
             as (hyr1 & hyr2 & HYP).
           rewrite HYP in H.
           apply H.
        -- intros.
           assert (hy2: length (a ::p) = length (m::ml)).
           {intros. simpl. rewrite hy. reflexivity. }
-          destruct (mk_rtc_def a p (phi_call (extract rcl) ps) m ml h h0 hy2 hyh) 
+          destruct (mk_rtc_def a p (phi_call (extract rcl) ps) m ml h h0 hy2 hyh)
              as (hyr1 & hyr2 & HYP).
           specialize (H0 (m::ml) hy2 H12 Hproc).
           specialize (HYP (fun l _ => Q l (m ::ml))).
@@ -118,7 +118,7 @@ induction p;intros.
      ** apply Hproc.
 Qed.
 
-(** Proof that one can use a verification condition 
+(** Proof that one can use a verification condition
     generator to proof relational procedure contract **)
 
 Lemma rcorrect_proc :
